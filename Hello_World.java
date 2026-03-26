9ccb6db676cac84e1180001086da4009bcf4a1cb
SAMPREETH-20 <shettysampreeth2@gmail.com>
2026-03-26 18:48:53 +0530

    Initial commit

---
+++ Hello_World.java
@@ -0,0 +1,9 @@
+package tns.demo;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ Strings_basics.java
@@ -0,0 +1,12 @@
+package tns.demo;
+
+public class String_basics {
+
+	public static void main(String[] args) {
+		String greeting="Hello World";
+		System.out.println("Original string "+greeting);
+		System.out.println("Uppercase "+greeting.toUpperCase());
+		System.out.println("Lowercase "+greeting.toLowerCase());
+	}
+
+}
